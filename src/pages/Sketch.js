01222c53b2b04acbb66a69e4010a00f078e9a3eb
Jym-lab <jym9809@gmail.com>
2023-09-06 11:43:17 +0900

    feat: switch guide

--- src/pages/Sketch.js
+++ src/pages/Sketch.js
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import SwitchBtn from "../component/SwitchBtn";
+import Game from '../pages/Game'
+import Joy from '../pages/Joy'
 
 const Sketch = () => {
     const [onActive, setOnActive] = useState('매표소')
+
+    let Content;
+
+    switch (onActive) {
+        // 콘텐츠 컴포넌트 추가해서 추가하면 됨
+        case '매표소':
+            Content = Game;
+            break;
+        case '본부':
+            Content = Joy;
+    }
     return (
         <>
             <div className="flex justify-center SwitchBtn OA">
@@ -19,6 +32,9 @@ const Sketch = () => {
             <div className="flex justify-center SwitchBtn OA">
                 <SwitchBtn character={'joy'} title={'장기자랑'} onActive={onActive} setOnActive={setOnActive}/>
                 <SwitchBtn character={'joy'} title={'동아리'} onActive={onActive} setOnActive={setOnActive}/>
+            </div>
+            <div>
+                <Content />
             </div>
         </>
     )
