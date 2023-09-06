52e8f41d9d3a25fc8fac68ad6465f722e1b61d85
seung-heee <tmdgmldi2595@naver.com>
2023-09-06 12:39:54 +0900

    sketch village 본부

--- src/component/SketchElement.js
+++ src/component/SketchElement.js
@@ -3,7 +3,7 @@ const SketchElement = ({ logo, tags, img, right }) => {
         <div className='SketchElement m-auto w-11/12 mt-6 last'>
             <div className={`box flex justify-evenly rounded-xl p-3 ${right ? 'flex-row-reverse' : ''}`}>
                 <div>
-                    <img src={process.env.PUBLIC_URL + `assets/sketch/${logo}.png`} alt="logoimg" />
+                    <img width="130" src={process.env.PUBLIC_URL + `assets/sketch/${logo}.png`} alt="logoimg" />
                     <p className={`mt-3 ${right ? 'mr-3 text-right' : 'ml-3'}`}>
                         {tags.map((tag) => {
                             return (
@@ -14,7 +14,7 @@ const SketchElement = ({ logo, tags, img, right }) => {
                         })}
                     </p>
                 </div>
-                <img width="180" src={process.env.PUBLIC_URL + `assets/sketch/${img}.png`} alt="" />
+                <img width="150" src={process.env.PUBLIC_URL + `assets/sketch/${img}.png`} alt="" />
             </div>
         </div>
     )
--- src/component/TicketBox.js
+++ src/component/TicketBox.js
@@ -3,7 +3,7 @@ import Subtitle from "./Subtitle";
 
 const TicketBox = ({ tags }) => {
     const missionTag = ['#미션지', '경품 추첨', '소니 헤드셋'];
-    const todayBoothTag = ['#어떤 동아리에 가입하지?', '동아리 랜덤 뽑기'];
+    const todayBoothTag = ['#어떤 동아리에', '가입하지?', '#동아리 랜덤 뽑기'];
 
     return (
         <div>
@@ -11,13 +11,13 @@ const TicketBox = ({ tags }) => {
                 <Subtitle character={'sketch'} title={'배부 물품'} direction={'left'} />
                 <div className="flex items-center justify-evenly">
                     <div className="flex flex-col items-center">
-                        <img width="130" src={process.env.PUBLIC_URL + 'assets/sketch/pamplet.png'} alt="" />
+                        <img width="130" src={process.env.PUBLIC_URL + 'assets/sketch/ticketBox/pamplet.png'} alt="" />
                         <p>팜플렛</p>
                     </div>
 
                     <div>
                         <div className="flex flex-col items-center">
-                            <img width="130" src={process.env.PUBLIC_URL + 'assets/sketch/monster.png'} alt="" />
+                            <img width="130" src={process.env.PUBLIC_URL + 'assets/sketch/ticketBox/monster.png'} alt="" />
                             <p>몬스터 음료</p>
                         </div>
                     </div>
@@ -27,8 +27,8 @@ const TicketBox = ({ tags }) => {
             <div>
                 <Subtitle character={'sketch'} title={'프로그램'} direction={'right'} />
                 <div>
-                    <SketchElement logo={'missionLogo'} tags={missionTag} img={'mission'} right={false} />
-                    <SketchElement logo={'todayBoothLogo'} tags={todayBoothTag} img={'todayBooth'} right={true} />
+                    <SketchElement logo={'ticketBox/missionLogo'} tags={missionTag} img={'ticketBox/mission'} right={false} />
+                    <SketchElement logo={'ticketBox/todayBoothLogo'} tags={todayBoothTag} img={'ticketBox/todayBooth'} right={true} />
                 </div>
             </div>
         </div>
