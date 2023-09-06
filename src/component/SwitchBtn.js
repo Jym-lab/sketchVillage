dafe7f20fcdde8d98c3963af626f7588946514ec
Jym-lab <jym9809@gmail.com>
2023-09-06 11:30:12 +0900

    feat: Add on off SwitchBtn

--- src/component/SwitchBtn.js
+++ src/component/SwitchBtn.js
@@ -1,8 +1,7 @@
-const SwitchBtn = ({ character, title }) => {
+const SwitchBtn = ({ character, title, onActive ,setOnActive }) => {
+    const isActive = onActive === title
     return (
-        <div className="SwitchBtn OA">
-            <button className={['text-xl', "border-b-4", "pb-2", "px-4", 'mx-2', 'my-5', `${character}_color`].join(' ')}>{title}</button>
-        </div>
+        <button onClick={() => setOnActive(title)} className={['text-xl', "border-b-4", "pb-2", "px-4", 'mx-2', 'my-5', `${character}_color${isActive ? '' : '_off'}`].join(' ')}>{title}</button>
     )
 }
 
--- src/pages/Sketch.js
+++ src/pages/Sketch.js
@@ -1,22 +1,24 @@
+import { useState } from "react";
 import SwitchBtn from "../component/SwitchBtn";
 
 const Sketch = () => {
+    const [onActive, setOnActive] = useState('매표소')
     return (
         <>
-            <div className="flex justify-center">
-                <SwitchBtn character={'sketch'} title={'매표소'} />
-                <SwitchBtn character={'sketch'} title={'본부'} />
+            <div className="flex justify-center SwitchBtn OA">
+                <SwitchBtn character={'sketch'} title={'매표소'} onActive={onActive} setOnActive={setOnActive}/>
+                <SwitchBtn character={'sketch'} title={'본부'} onActive={onActive} setOnActive={setOnActive}/>
             </div>
 
-            <div className="flex justify-center">
-                <SwitchBtn character={'neighbor'} title={'부스'} />
-                <SwitchBtn character={'neighbor'} title={'협찬'} />
-                <SwitchBtn character={'neighbor'} title={'학생부스'} />
+            <div className="flex justify-center SwitchBtn OA">
+                <SwitchBtn character={'neighbor'} title={'부스'} onActive={onActive} setOnActive={setOnActive}/>
+                <SwitchBtn character={'neighbor'} title={'협찬'} onActive={onActive} setOnActive={setOnActive}/>
+                <SwitchBtn character={'neighbor'} title={'학생부스'} onActive={onActive} setOnActive={setOnActive}/>
             </div>
 
-            <div className="flex justify-center">
-                <SwitchBtn character={'joy'} title={'장기자랑'} />
-                <SwitchBtn character={'joy'} title={'동아리'} />
+            <div className="flex justify-center SwitchBtn OA">
+                <SwitchBtn character={'joy'} title={'장기자랑'} onActive={onActive} setOnActive={setOnActive}/>
+                <SwitchBtn character={'joy'} title={'동아리'} onActive={onActive} setOnActive={setOnActive}/>
             </div>
         </>
     )
