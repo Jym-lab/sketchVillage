a3f19d99414931d9daf26e66dddcb0aee4e06376
seung-heee <tmdgmldi2595@naver.com>
2023-09-06 23:36:48 +0900

    neighbor OutSponsor

--- src/component/neighbor/OutBooth.js
+++ src/component/neighbor/OutBooth.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import ImgSubtitle from "../ImgSubtitle";
 import Subtitle from "../Subtitle";
-import { SlActionRedo } from "react-icons/sl";
+import { HiMiniCursorArrowRays } from "react-icons/hi2";
 
 const OutBooth = () => {
     return (
@@ -9,40 +9,42 @@ const OutBooth = () => {
             <div>
                 <ImgSubtitle character={'neighbor'} title={'neighbor/sangsangUniv'} direction={'left'} />
                 <div className="neighborElement Element">
-                    <div className="box  rounded-2xl p-5 pl-12">
+                    <div className="box rounded-2xl p-5 pl-12">
                         <ul class="list-none">
-                            <li className="font-semibold text-gray-700 mb-1">
+                            <li className="text-gray-700 mb-1">
                                 <Link to="https://www.ktng.com/univ" className="flex items-center">
-                                    1.<span className="mr-1  underline underline-offset-4 italic">해당링크로 참여신청</span><SlActionRedo /></Link></li>
+                                    1.<span className="mr-1 underline underline-offset-4 italic font-bold">해당링크로 참여신청</span>
+                                    <HiMiniCursorArrowRays /></Link></li>
                             <li className="NeighborsmallTag mb-4">#공책 #마스킹테이프 #스티커 #선정리클립</li>
-                            <li className="font-semibold text-gray-700  mb-1">2. 주사위던지기 이벤트 진행</li>
+                            <li className=" text-gray-700  mb-1">2. 주사위던지기 이벤트 진행</li>
                             <li className="NeighborsmallTag">#코닥일회용카메라 #마우스패드 #링티</li>
                         </ul>
                     </div>
                 </div>
-
             </div>
+
             <div className='relative'>
                 <Subtitle character={'neighbor'} title={'IT\'S ME'} direction={'left'} />
                 <img width="100" className="absolute top-12 right-0" src={process.env.PUBLIC_URL + `assets/neighbor/itsMe.png`} alt="생리대" />
                 <div className="neighborElement Element">
                     <div className="box  rounded-2xl p-5 pl-12">
                         <ul class="list-none">
-                            <li className="font-semibold text-gray-700 mb-1">1. 룰렛이벤트 진행</li>
+                            <li className=" text-gray-700 mb-1">1. 룰렛이벤트 진행</li>
                             <li className="NeighborsmallTag mb-4">다양한 <span className="font-bold">생리대 세트</span> 증정</li>
-                            <li className="font-semibold text-gray-700  mb-1">2. 부스 방문시 <span className="italic neighborfontcolor">잇츠미 생리대</span> 증정</li>
+                            <li className=" text-gray-700  mb-1">2. 부스 방문시 <span className="font-bold neighborfontcolor">잇츠미 생리대</span> 증정</li>
                         </ul>
                     </div>
                 </div>
             </div>
+
             <div className="relative last">
                 <Subtitle character={'neighbor'} title={'도로시팩토리'} direction={'left'} />
                 <img width="100" className="absolute top-12 -right-2" src={process.env.PUBLIC_URL + `assets/neighbor/kiring.png`} alt="키링" />
                 <div className="neighborElement Element">
                     <div className="box  rounded-2xl p-5 pl-12">
                         <ul class="list-none">
-                            <li className="font-semibold text-gray-700 mb-1">
-                                <span className="neighborfontcolor">레진 아트</span> (키링, 스트랩 등)<br />
+                            <li className="text-gray-700 mb-1">
+                                <span className="neighborfontcolor font-bold">레진 아트</span> (키링, 스트랩 등)<br />
                                 10 ~ 20% 할인된 가격에 판매
                             </li>
                         </ul>
--- src/component/neighbor/OutSponsor.js
+++ src/component/neighbor/OutSponsor.js
@@ -1,12 +1,57 @@
 import ImgSubtitle from "../ImgSubtitle";
+import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
+import { SiKakaotalk } from "react-icons/si";
+import { BsInstagram } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { HiMiniCursorArrowRays } from "react-icons/hi2";
 
 const OutSponsor = () => {
     return (
         <div className="OutSponsor slide-in">
-            <ImgSubtitle character={'neighbor'} title={'neighbor/monsterLogo'} direction={'left'} />
-            <ImgSubtitle character={'neighbor'} title={'neighbor/bacchusLogo'} direction={'left'} />
-            <ImgSubtitle character={'neighbor'} title={'neighbor/yvesomLogo'} direction={'left'} />
-        </div>
+            <div>
+                <ImgSubtitle character={'neighbor'} title={'neighbor/monsterLogo'} direction={'left'} />
+
+                <div className="neighborElement Element">
+                    <div className="box rounded-2xl p-5 flex justify-evenly items-center text-center">
+                        <div>동아리페스티벌<br />참여자 전체<br /><span className="neighborfontcolor font-bold">몬스터 음료</span> 증정</div>
+                        <img width='130' src={process.env.PUBLIC_URL + `assets/neighbor/monster1.png`} alt="몬스터" />
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <ImgSubtitle character={'neighbor'} title={'neighbor/bacchusLogo'} direction={'left'} />
+
+                <div className="neighborElement Element">
+                    <div className="box rounded-2xl p-5 flex flex-row-reverse justify-evenly items-center">
+                        <ul className="flex flex-col justify-center items-center text-center">
+                            <li className="mb-1 underline underline-offset-4 italic font-bold"><Link to="http://pf.kakao.com/_vxixlaxj" className="flex items-center">
+                                <BsInstagram size='18' className="mr-1" />인스타 팔로우<HiMiniCursorArrowRays className="ml-1" /> </Link></li>
+                            <li className="mb-1"><MdOutlineKeyboardDoubleArrowDown /></li>
+                            <li className="neighborfontcolor">박카스맛 젤리 증정</li>
+                        </ul>
+                        <img width='130' src={process.env.PUBLIC_URL + `assets/neighbor/bacchus.png`} alt="박카스" />
+                    </div>
+                </div>
+            </div >
+
+            <div>
+                <ImgSubtitle character={'neighbor'} title={'neighbor/yvesomLogo'} direction={'left'} />
+
+                <div className="neighborElement Element last">
+                    <div className="box rounded-2xl p-5 flex justify-evenly items-center">
+                        <ul className="flex flex-col justify-center items-center text-center">
+                            <li className="mb-1 underline underline-offset-4 italic font-bold"><Link to="http://pf.kakao.com/_vxixlaxj" className="flex items-center ">
+                                <SiKakaotalk size='18' className="mr-1" />입솜 카톡채널 추가<HiMiniCursorArrowRays className="ml-1" /></Link></li>
+                            <li className="mb-1"><MdOutlineKeyboardDoubleArrowDown /></li>
+                            <li className="mb-2"><span className="neighborfontcolor font-bold italic">오일프리 모이스처라이저</span> 증정</li>
+                            <li className="NeighborsmallTag">+ '화해'어플로 <span className="font-bold">리뷰 작성시 토너 추가 증정</span>(캡쳐사진 요청 예정) </li>
+                        </ul>
+                        <img width='110' src={process.env.PUBLIC_URL + `assets/neighbor/yvesom.png`} alt="몬스터" />
+                    </div>
+                </div >
+            </div >
+        </div >
     )
 }
 
