52f2a5ee85d7c1a9fcca20d6751ab55c15f93453
seung-heee <tmdgmldi2595@naver.com>
2023-09-13 00:43:48 +0900

    수정

--- src/component/nav/Nav.js
+++ src/component/nav/Nav.js
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ShowNav from "./ShowNav";
 import { BiHomeAlt } from "react-icons/bi";
 const Nav = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const [scrolling, setScrolling] = useState(false); // 스크롤 상태를 추적하기 위한 상태 변수
+
+    useEffect(() => {
+        // 스크롤 이벤트 핸들러를 추가
+        const handleScroll = () => {
+            if (window.scrollY > 300) {
+                setScrolling(true); // 스크롤 위치가 300px 이상이면 scrolling 상태를 true로 설정
+            } else {
+                setScrolling(false); // 그 외의 경우에는 scrolling 상태를 false로 설정
+            }
+        };
+
+        // 컴포넌트가 마운트될 때 스크롤 이벤트 리스너 추가
+        window.addEventListener("scroll", handleScroll);
+
+        // 언마운트될 때 스크롤 이벤트 리스너 제거
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    // Navbar의 클래스 이름을 동적으로 설정하고 scrolling 상태에 따라 배경색 클래스를 추가
+    const navbg = `Nav fixed top-0 flex justify-between items-center px-5 py-3 ${isOpen ? 'z-40' : 'z-30'} ${scrolling ? 'nav-bg-scrolled' : ''}`;
+
     return (
         <div>
-            <div className={`Nav w-96 fixed top-0 flex justify-between items-center px-5 pt-2 ${isOpen ? 'z-40' : 'z-30'}`}>
+            <div className={navbg}>
                 <Link className={`${isOpen ? '' : 'z-20'}`} to="/" onClick={() => {
                     window.scrollTo({
                         top: 0,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
             <div className="Main min-h-screen flex flex-col">
                 <Nav />
                 <CloudLottie option={'absolute top-1/3'} />
-                <div className="flex flex-col items-center justify-center flex-1">
-                    <div className="tracking-in-expand-fwd-top text-xl font-bold titlegreen ">2023 동아리페스티벌</div>
+                <div className="flex flex-col items-center justify-center flex-1 pt-12">
+                    <div className="tracking-in-expand-fwd-top text-xl font-bold titlegreen">2023 동아리페스티벌</div>
                     <div><img src={process.env.PUBLIC_URL + `assets/Logo/Logo.png`} alt="" /></div>
                     <div className='tracking-in-contract-bck-bottom mb-6 text-base font-bold titlegreen'>2023.09.19 성결대학교 운동장</div>
                 </div>
